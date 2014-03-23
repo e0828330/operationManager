7604d5dbf93a1966a531612c025d197d48589944
Adel Gadllah <adel.gadllah@gmail.com>
2014-03-23 13:59:29 +0100

    Use service in rest

--- operationManager/src/main/java/restService/MainController.java
+++ operationManager/src/main/java/restService/MainController.java
@@ -1,21 +1,28 @@
 package restService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import service.TestService;
+
 @Controller
 public class MainController {
 
-	 @RequestMapping("/rest")
-	 public @ResponseBody String root() {
-		 System.out.println("CALLED!!!!");
-		 return "Test";
-	 }
-	 
-	 @RequestMapping("/rest/bla")
-	 public @ResponseBody String test2() {
-		 System.out.println("CALLED!!!!");
-		 return "Bla";
-	 }
+	@Autowired
+	private TestService service;
+
+	@RequestMapping("/rest")
+	public @ResponseBody
+	String root() {
+		return service.getString();
+	}
+
+	@RequestMapping("/rest/bla")
+	public @ResponseBody
+	String test2() {
+		System.out.println("CALLED!!!!");
+		return "Bla";
+	}
 }
