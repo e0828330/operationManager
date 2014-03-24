e6a28b2ce6a9c0b755b9967af4187a6e1933c2d8
Adel Gadllah <adel.gadllah@gmail.com>
2014-03-24 13:21:05 +0100

    Fix PatientRepo type

--- operationManager/src/main/java/repository/PatientRepository.java
+++ operationManager/src/main/java/repository/PatientRepository.java
@@ -2,10 +2,12 @@ package repository;
 
 import model.Patient;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends PagingAndSortingRepository<Patient, Long> {
+@Lazy
+public interface PatientRepository extends PagingAndSortingRepository<Patient, String> {
 
 }
