27a47797aa557e54f6d61aabf8365f5c89cfd15c
Adel Gadllah <adel.gadllah@gmail.com>
2014-03-27 19:59:28 +0100

    model: Add id fields

--- operationManager/src/main/java/model/Patient.java
+++ operationManager/src/main/java/model/Patient.java
@@ -1,11 +1,18 @@
 package model;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 import org.springframework.data.annotation.Id;
 
 @Data
-public class Patient {
+public class Patient implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5300740584902282823L;
+
 	@Id
 	private String id;
 
