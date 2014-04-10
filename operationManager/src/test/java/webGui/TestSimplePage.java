b5bffe37b06815be9300e468e8d17698b5341053
Adel Gadllah <adel.gadllah@gmail.com>
2014-04-10 21:43:42 +0200

    Setup webapp as website root for static resources

--- operationManager/src/test/java/webGui/TestSimplePage.java
+++ operationManager/src/test/java/webGui/TestSimplePage.java
@@ -1,35 +1,7 @@
 package webGui;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.swing.Spring;
-
-import model.OPSlot;
-
-import org.apache.wicket.spring.SpringWebApplicationFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.springframework.asm.SpringAsmInfo;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericApplicationContext;
-
-import service.IOPSlotService;
-import service.real.OPSlotService;
 
 public class TestSimplePage {
 	
@@ -39,7 +11,7 @@ public class TestSimplePage {
 		 
         WebAppContext webapp = new WebAppContext();
         webapp.setContextPath("/test");
-        webapp.setWar("src/main/webtest");
+        webapp.setWar("src/main/webapp");
         server.setHandler(webapp);
  
 		server.start();
