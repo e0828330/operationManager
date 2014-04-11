9812e68af471891844a92b92648d2b189d45fe89
Adel Gadllah <adel.gadllah@gmail.com>
2014-04-11 18:10:10 +0200

    Fix warnings

--- operationManager/src/main/java/repository/PatientRepository.java
+++ operationManager/src/main/java/repository/PatientRepository.java
@@ -2,9 +2,7 @@ package repository;
 
 import model.Patient;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PatientRepository extends PagingAndSortingRepository<Patient, String> {
 
