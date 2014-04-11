e5f9a760401cd25a4f067f66b93ecbf898e4b9df
Adel Gadllah <adel.gadllah@gmail.com>
2014-04-11 18:07:46 +0200

    Fix warnings

--- operationManager/src/main/java/repository/HospitalRepository.java
+++ operationManager/src/main/java/repository/HospitalRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import model.Hospital;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.core.geo.Distance;
 import org.springframework.data.mongodb.core.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 public interface HospitalRepository  extends MongoRepository<Hospital, String> {
 	 List<Hospital> findByPositionNear(Point p, Distance d);
