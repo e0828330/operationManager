06e137f527d7ad4894ef6fb4a48f0c527b91aa53
Adel Gadllah <adel.gadllah@gmail.com>
2014-04-29 19:44:28 +0200

    Switch to app config and enable opSlot service

--- operationManager/src/main/java/service/real/OPSlotService.java
+++ operationManager/src/main/java/service/real/OPSlotService.java
@@ -1,17 +1,30 @@
 package service.real;
 
+import java.util.Date;
 import java.util.List;
 
+import model.Doctor;
+import model.Hospital;
 import model.OPSlot;
+import model.OperationStatus;
+import model.OperationType;
+import model.Patient;
 import model.dto.OPSlotFilter;
 
 import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import repository.OPSlotRepository;
 import service.IOPSlotService;
 
 @Service
 public class OPSlotService implements IOPSlotService {
+	
+	@Autowired
+	private OPSlotRepository repo;
+
 	
 	@Override
 	public List<OPSlot> getOPSlots(SortParam<String> sort, OPSlotFilter filter, long first, long count) {
@@ -20,6 +33,26 @@ public class OPSlotService implements IOPSlotService {
 
 	@Override
 	public long getOPSlotCount(OPSlotFilter filter) {
+		/*
+		OPSlot slot = new OPSlot();
+		
+		Patient patient = new Patient();
+		patient.setFirstName("Adelheid");
+		patient.setLastName("Abesser");
+		slot.setPatient(patient);
+		
+		Hospital hs = new Hospital();
+		hs.setName("SMZ Ost");
+		slot.setHospital(hs);
+
+		slot.setDate(new Date());
+		slot.setFrom(new Date());
+		slot.setTo(new Date(new Date().getTime() + 3600000));
+		slot.setType(OperationType.eye);
+		slot.setStatus(OperationStatus.reserved);
+		
+		repo.save(slot);*/
+			
 		return 0L;
 	}
 }
