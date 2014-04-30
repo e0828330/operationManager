2abee652a9ee51bdbe072139c3e6d886eaa28ed8
martin <ms0828600@gmail.com>
2014-04-30 14:08:17 +0200

    Fix injection für authentication service

--- operationManager/src/main/java/service/real/AuthenticationService.java
+++ operationManager/src/main/java/service/real/AuthenticationService.java
@@ -2,6 +2,7 @@ package service.real;
 
 import model.Role;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import service.IAuthenticationService;
@@ -9,9 +10,13 @@ import service.IAuthenticationService;
 @Service
 public class AuthenticationService implements IAuthenticationService {
 
+	@Bean
+	static IAuthenticationService getAuthenticationService() {
+		return new AuthenticationService();
+	}	
+	
 	@Override
 	public Role authenticate(String username, String password) {
-		
 		return Role.DOCTOR;
 	}
 	
