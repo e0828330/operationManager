4bd73daa538742b636e091a15fa927912ac0c4ce
Adel Gadllah <adel.gadllah@gmail.com>
2014-05-03 09:34:43 +0200

    Remove async tests again

--- operationManager/src/test/java/georesolver/QueueTest.java
+++ operationManager/src/test/java/georesolver/QueueTest.java
@@ -1,17 +1,14 @@
 package georesolver;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,48 +23,15 @@ public class QueueTest {
 	
 	@Test
 	public void testGeoResolverQueue() {
-		rabbitTemplate.convertAndSend("geoResolverQueue", "hello");
+		rabbitTemplate.convertAndSend("geoResolverQueue", "hello-testGeoResolverQueue");
 		Object message = rabbitTemplate.receiveAndConvert("geoResolverQueue");
-		assertEquals("hello", (String) message);
+		assertEquals("hello-testGeoResolverQueue", (String) message);
 	}
 
 	@Test
 	public void testNewsBeeperQueue() {
-		rabbitTemplate.convertAndSend("newsBeeperQueue", "hello");
+		rabbitTemplate.convertAndSend("newsBeeperQueue", "hello-testNewsBeeperQueue");
 		Object message = rabbitTemplate.receiveAndConvert("newsBeeperQueue");
-		assertEquals("hello", (String) message);
-	}
-	
-	@Test
-	public void asyncGeoQueueTest() {
-		final SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames("geoResolverQueue");
-		container.setMessageListener(new MessageListener() {
-			@Override
-			public void onMessage(Message msg) {
-				assertEquals("hello", new String(msg.getBody()));
-				container.stop();
-			}
-		});
-		container.start();
-		rabbitTemplate.convertAndSend("geoResolverQueue", "hello");
-	}
-	
-	
-	@Test
-	public void asyncNewsQueueTest() {
-		final SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames("newsBeeperQueue");
-		container.setMessageListener(new MessageListener() {
-			@Override
-			public void onMessage(Message msg) {
-				assertEquals("hello", new String(msg.getBody()));
-				container.stop();
-			}
-		});
-		container.start();
-		rabbitTemplate.convertAndSend("newsBeeperQueue", "hello");
+		assertEquals("hello-testNewsBeeperQueue", (String) message);
 	}
 }
