750c0f6c9eb6e527fb05670d27ce73b516905a56
martin <ms0828600@gmail.com>
2014-05-16 15:44:18 +0200

    Add jms-service class

---
+++ operationManager/src/main/java/service/jms/JMSService.java
@@ -0,0 +1,29 @@
+package service.jms;
+
+import java.util.Queue;
+
+import model.OPSlot;
+import service.IQueueListener;
+import service.IQueueService;
+
+public class JMSService implements IQueueService {
+
+	@Override
+	public void sendToGeoResolver(OPSlot slot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendToNewsBeeper(OPSlot slot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerListener(String queueName, IQueueListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
