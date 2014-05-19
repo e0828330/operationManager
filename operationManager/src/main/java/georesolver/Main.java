2cfb7b7740468d353e7a87739d9adfb2b7c3d6e4
Adel Gadllah <adel.gadllah@gmail.com>
2014-05-19 20:33:59 +0200

    georesolver: Fix warnings

--- operationManager/src/main/java/georesolver/Main.java
+++ operationManager/src/main/java/georesolver/Main.java
@@ -1,6 +1,5 @@
 package georesolver;
 
-import model.OPSlot;
 import model.dto.Message;
 import model.dto.OPSlotDTO;
 
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -27,34 +25,34 @@ public class Main implements InitializingBean {
 	@Autowired
 	IOPSlotService opSlotService;
 
+	private static ApplicationContext context;
+
 	@Override
 	public void afterPropertiesSet() throws Exception {		
 	}
 	
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("/spring/test-config.xml");
+		context = new ClassPathXmlApplicationContext("/spring/test-config.xml");
 		Main prog = new Main();
 		AutowireCapableBeanFactory factory = context.getAutowireCapableBeanFactory();
 		factory.autowireBeanProperties(prog, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
 		factory.initializeBean(prog, "georesolver");
 		prog.run();
-		((ConfigurableApplicationContext)context).close();
 	}
 	
 	private void registerListener() {
 		// Register georesolver async listener
 		queueService.registerListener(RabbitMQConfig.GEORESOLVER_Q, new IQueueListener() {
-			
 			@Override
 			public void handleMessage(Message m) {
 				OPSlotDTO slot = (OPSlotDTO) m;
+				System.out.println(slot);
 			}
 		});		
 	}
 
 	
 	void run() {
-		System.out.println(queueService); // Just a test
-		System.out.println(opSlotService); // Just a test
+		registerListener();
 	}
 }
