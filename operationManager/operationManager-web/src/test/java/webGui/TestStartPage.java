42f6c5d94056562995b4ffff25e19c1b519c494b
Molotovski <florian.cepak@aon.at>
2014-06-01 17:07:40 +0200

    add tests for filter components

--- operationManager/operationManager-web/src/test/java/webGui/TestStartPage.java
+++ operationManager/operationManager-web/src/test/java/webGui/TestStartPage.java
@@ -1,9 +1,17 @@
 package webGui;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.tester.FormTester;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Test;
+
+import com.googlecode.wicket.kendo.ui.form.datetime.DatePicker;
+import com.googlecode.wicket.kendo.ui.form.datetime.TimePicker;
 
 public class TestStartPage extends AbstractBaseTest {
 
@@ -22,12 +30,98 @@ public class TestStartPage extends AbstractBaseTest {
         server.join();
 	}
 	
+	/**
+	 * asserts the necessary form components, fills the user name and a password, and submits the form
+	 * @param user
+	 */
+	private void login(String user) {
+		tester.assertComponent("loginForm:username", TextField.class);
+		tester.assertComponent("loginForm:password", PasswordTextField.class);
+		
+		FormTester formTester = tester.newFormTester("loginForm");
+		formTester.setValue("username", user);
+		formTester.setValue("password", "a");
+		formTester.submit();
+	}
+	
 	@Test
-	public void should_render_table() {
+	public void page_renders() {
 		tester.startPage(StartPage.class);
 		
 		tester.assertRenderedPage(StartPage.class);
+	}
+	
+	@Test
+	public void table_renders() {
+		page_renders();
 		
 		tester.assertComponent("overview:overviewTable", DataTable.class);
+	}
+	
+	@Test
+	public void all_filter_components_render_role_default() {
+		page_renders();
+		
+		tester.assertComponent("overview:filterForm", Form.class);
+		
+		tester.assertComponent("overview:filterForm:date", DatePicker.class);
+		tester.assertComponent("overview:filterForm:from", TimePicker.class);
+		tester.assertComponent("overview:filterForm:to", TimePicker.class);
+		tester.assertInvisible("overview:filterForm:patient");
+		tester.assertComponent("overview:filterForm:hospital", TextField.class);
+		tester.assertComponent("overview:filterForm:doctor", TextField.class);
+		tester.assertComponent("overview:filterForm:type", DropDownChoice.class);
+		tester.assertComponent("overview:filterForm:status", DropDownChoice.class);
+	}
+	
+	@Test
+	public void all_filter_components_render_role_patient() {
+		page_renders();
+		login("patient");
+		
+		tester.assertComponent("overview:filterForm", Form.class);
+		
+		tester.assertComponent("overview:filterForm:date", DatePicker.class);
+		tester.assertComponent("overview:filterForm:from", TimePicker.class);
+		tester.assertComponent("overview:filterForm:to", TimePicker.class);
+		tester.assertInvisible("overview:filterForm:patient");
+		tester.assertComponent("overview:filterForm:hospital", TextField.class);
+		tester.assertComponent("overview:filterForm:doctor", TextField.class);
+		tester.assertComponent("overview:filterForm:type", DropDownChoice.class);
+		tester.assertInvisible("overview:filterForm:status");
+	}
+	
+	@Test
+	public void all_filter_components_render_role_hospital() {
+		page_renders();
+		login("hospital");
+		
+		tester.assertComponent("overview:filterForm", Form.class);
+		
+		tester.assertComponent("overview:filterForm:date", DatePicker.class);
+		tester.assertComponent("overview:filterForm:from", TimePicker.class);
+		tester.assertComponent("overview:filterForm:to", TimePicker.class);
+		tester.assertComponent("overview:filterForm:patient", TextField.class);
+		tester.assertInvisible("overview:filterForm:hospital");
+		tester.assertComponent("overview:filterForm:doctor", TextField.class);
+		tester.assertComponent("overview:filterForm:type", DropDownChoice.class);
+		tester.assertInvisible("overview:filterForm:status");
+	}
+	
+	@Test
+	public void all_filter_components_render_role_doctor() {
+		page_renders();
+		login("doctor");
+		
+		tester.assertComponent("overview:filterForm", Form.class);
+		
+		tester.assertComponent("overview:filterForm:date", DatePicker.class);
+		tester.assertComponent("overview:filterForm:from", TimePicker.class);
+		tester.assertComponent("overview:filterForm:to", TimePicker.class);
+		tester.assertComponent("overview:filterForm:patient", TextField.class);
+		tester.assertComponent("overview:filterForm:hospital", TextField.class);
+		tester.assertInvisible("overview:filterForm:doctor");
+		tester.assertComponent("overview:filterForm:type", DropDownChoice.class);
+		tester.assertInvisible("overview:filterForm:status");
 	}
 }
