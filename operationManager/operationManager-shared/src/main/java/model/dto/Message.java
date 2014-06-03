0bcf2001e141f983d91fbd6201f804a1856440f5
Adel Gadllah <adel.gadllah@gmail.com>
2014-06-03 20:42:40 +0200

    Fix warnings

--- operationManager/operationManager-shared/src/main/java/model/dto/Message.java
+++ operationManager/operationManager-shared/src/main/java/model/dto/Message.java
@@ -1,15 +1,6 @@
 package model.dto;
 
 import java.io.Serializable;
-import java.util.Date;
-
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Setter;
-import model.OperationType;
-import model.Patient;
-
-import org.springframework.data.annotation.Id;
 
 public class Message implements Serializable {
 
--- operationManager/operationManager-shared/src/test/java/queue/QueueTest.java
+++ operationManager/operationManager-shared/src/test/java/queue/QueueTest.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import service.real.QueueService;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring/application-config.xml" })
