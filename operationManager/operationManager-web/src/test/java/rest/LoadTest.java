7743aa841f0e4e51fbd6c654339396db4f4f2bb2
Adel Gadllah <adel.gadllah@gmail.com>
2014-06-06 21:01:25 +0200

    web: Add loadTest
    The load test floods the application with multiple reservation requests
    in parallel. This should stress all parts of the application because the
    web component has to handle the request, validate it and pass it on to
    the georesolver, which has to validate it, try to find a suitable slot
    write the result to the database of any and inform the newsbeeper. The
    newsbeeper has to validate the request and store the resulting
    notifications in the database.
    
    This generates load on all components including the database (data
    retrival and storage) and the MoM (sending of messages between
    components).

---
+++ operationManager/operationManager-web/src/test/java/rest/LoadTest.java
@@ -0,0 +1,73 @@
+package rest;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import model.OperationType;
+import model.dto.RestPatientDTO;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class LoadTest {
+
+	private final int numberOfRequests = 1024;
+	
+	private List<RestPatientDTO> getPatients() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();  
+		String json = given().param("username", "maria").param("password", "test09").get("operationManager-web/rest/getPatients/").asString();
+		List<RestPatientDTO> result = mapper.readValue(json, new TypeReference<List<RestPatientDTO>>() {});
+		return result;
+	}
+	
+	private class ReservationWorker implements Runnable {
+
+		private String patientId;
+		private String operationType;
+		
+		public ReservationWorker(String patientId, String operationType) {
+			this.patientId = patientId;
+			this.operationType = operationType;
+		}
+		
+		@Override
+		public void run() {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+			String result = given().param("username", "maria").
+								param("password", "test09").
+								param("patientId", patientId).
+								param("operationType", operationType).
+								param("from", dateFormat.format(new Date())).
+								param("to", "01.01.2016").
+								param("distance", new Integer((int) (Math.random() * 150)).toString()).
+						    post("/operationManager-web/rest/reserveOPSlot/").asString();
+
+			// FIXME: Do something useful with the result?
+			System.err.println(result);
+		}
+	}
+	
+	@Test
+	public void loadTest() throws JsonParseException, JsonMappingException, IOException, InterruptedException {
+		ExecutorService executor = Executors.newCachedThreadPool();
+		List<RestPatientDTO> patients = getPatients();
+		for (int i = 0; i < numberOfRequests; i++) {
+			RestPatientDTO p = patients.get(i % patients.size());
+			String type = OperationType.values()[i % OperationType.values().length].toString();
+			executor.execute(new ReservationWorker(p.getId(), type));
+		}
+		executor.shutdown();
+		executor.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+	}
+}
